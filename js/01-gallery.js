7cb4dbed59ed731f1c7c46a6d87a19fed646c32e
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-04-03 11:24:51 +0300

    function of markup created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMainBox = document.body.querySelector('.gallery');
+const galleryMarkupCreaton = createGalleryFromObject(galleryItems);
+
+galleryMainBox.insertAdjacentHTML('beforeend', galleryMarkupCreaton);
+
+function createGalleryFromObject(obj) {
+    return obj.map(({ original, preview, description}) => `<div class="gallery__item">
+    <a class="gallery__link" href = '${original}' >
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="large-image.jpg"
+                    alt="${description}"
+                    target="_self";
+                />
+            </a>
+            </div >`).join('');
+}
+
+galleryMainBox.addEventListener('click', onGalleyItemClick());
+
+function onGalleyItemClick(evt) {
+
+};
