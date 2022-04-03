38dd21b0eacf0affb882ecd1951788b93a958f05
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-04-03 15:18:03 +0300

    finish 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,20 +8,46 @@ galleryMainBox.insertAdjacentHTML('beforeend', galleryMarkupCreaton);
 
 function createGalleryFromObject(obj) {
     return obj.map(({ original, preview, description}) => `<div class="gallery__item">
-    <a class="gallery__link" href = '${original}' >
-                <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="large-image.jpg"
-                    alt="${description}"
-                    target="_self";
-                />
-            </a>
-            </div >`).join('');
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
 }
 
-galleryMainBox.addEventListener('click', onGalleyItemClick());
+
+galleryMainBox.addEventListener('click', onGalleyItemClick);
+galleryMainBox.addEventListener('keydown', onEscapeButtonPress);
 
 function onGalleyItemClick(evt) {
+    evt.preventDefault();
 
+    const targetedImageInGallery = evt.target;
+    if (!targetedImageInGallery.classList.contains('gallery__image')) {
+        return
+    }
+ 
+    creationOfModalImage(targetedImageInGallery);
+        
 };
+
+function creationOfModalImage(item) {
+    const originalItemSource = item.dataset.source;
+    const instance = basicLightbox.create(`<img src=${originalItemSource} class="basicLightbox__image" width="800" height="600">
+`)
+
+    instance.show();
+}
+
+function onEscapeButtonPress(evt) {
+    if (!evt.code === 'Escape') {
+        return
+    }
+
+    const modalChildInBody = document.body.lastChild;
+    modalChildInBody.remove();
+}
