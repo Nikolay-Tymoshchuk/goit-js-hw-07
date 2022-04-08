c5e56ccb86ff8cbb82761e2a33d36dc5d1b6b4b6
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-04-08 16:48:37 +0300

    fixed some remarks from mentor

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryMainBox = document.body.querySelector('.gallery');
+const galleryMainBox = document.body.querySelector(".gallery");
 const galleryMarkupCreaton = createGalleryFromObject(galleryItems);
 
-galleryMainBox.insertAdjacentHTML('beforeend', galleryMarkupCreaton);
+galleryMainBox.insertAdjacentHTML("beforeend", galleryMarkupCreaton);
 
 function createGalleryFromObject(obj) {
-    return obj.map(({ original, preview, description }) =>
-    `<div class="gallery__item">
+  return obj
+    .map(
+      ({ original, preview, description }) =>
+        `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -17,41 +19,37 @@ function createGalleryFromObject(obj) {
       alt="${description}"
     />
   </a>
-</div>`).join('');
+</div>`
+    )
+    .join("");
 }
 
-
-galleryMainBox.addEventListener('click', onGalleyItemClick);
-
+galleryMainBox.addEventListener("click", onGalleyItemClick);
 
 function onGalleyItemClick(evt) {
-    evt.preventDefault();
-    galleryMainBox.addEventListener('keydown', onEscapeButtonCloseModal);
+  evt.preventDefault();
 
-    const targetedImageInGallery = evt.target;
-    if (!targetedImageInGallery.classList.contains('gallery__image')) {
-        return
-    }
- 
-    creationOfModalImage(targetedImageInGallery);
-        
-};
+  const targetedImageInGallery = evt.target;
+  if (!targetedImageInGallery.classList.contains("gallery__image")) {
+    return;
+  }
+
+  creationOfModalImage(targetedImageInGallery);
+  galleryMainBox.addEventListener("keydown", onEscapeButtonCloseModal);
+}
 
 function creationOfModalImage(item) {
-    const originalItemSource = item.dataset.source;
-    const instance = basicLightbox.create(`<img src=${originalItemSource} class="basicLightbox__image" width="800" height="600">
-`)
+  const originalItemSource = item.dataset.source;
+  const instance =
+    basicLightbox.create(`<img src=${originalItemSource} class="basicLightbox__image" width="800" height="600">
+`);
 
-    instance.show();
+  instance.show();
 }
 
 function onEscapeButtonCloseModal(evt) {
-    const modalChildInBody = document.body.querySelector('.basicLightbox');
-    if (!modalChildInBody) {
-        return;
-    }
-    else if (evt.code !== 'Escape') {
-        return;
-    }
-    else modalChildInBody.remove();     
+  const modalWindow = document.body.querySelector(".basicLightbox");
+  if (evt.code !== "Escape") {
+    return;
+  } else modalWindow.remove();
 }
